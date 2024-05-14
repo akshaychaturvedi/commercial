84c0f13d5434be0cabae5ac8d51948c83ad23d2f
Akshay Chaturvedi <akshaychaturvedi129@gmail.com>
2024-05-14 18:23:09 +0000

    addonui

---
+++ commercial-ui/app/addon-ui/webapp/controller/App.controller.js
@@ -0,0 +1,72 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/Filter", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, Filter, MessageToast) {
+    "use strict";
+
+    return Controller.extend("addonui.controller.App", {
+      onInit: function () {},
+
+      filterTable: async function (oTable, sProductKey) {
+        var oBinding = oTable.getBinding("items");
+        var oFilters = [
+          new sap.ui.model.Filter(
+            "product_ID",
+            sap.ui.model.FilterOperator.EQ,
+            sProductKey
+          ),
+          new sap.ui.model.Filter(
+            "isChecked",
+            sap.ui.model.FilterOperator.EQ,
+            true
+          ),
+        ];
+
+        return new Promise(function (resolve, reject) {
+          // Perform filtering
+          oBinding.filter(oFilters,'Application');
+
+          // Resolve the Promise after the filtering is complete
+          oBinding.attachEventOnce("dataReceived", function () {
+            resolve();
+          });
+        });
+      },
+
+      onSelection: async function () {
+        var oProductBox = this.getView().byId("productBox");
+        var oSelectedProduct = oProductBox.getSelectedItem();
+        if (oSelectedProduct) {
+          var sProductKey = oSelectedProduct.getKey();
+        } else {
+          MessageToast.show("Select Product");
+          return;
+        }
+
+        var oLanguageBox = this.getView().byId("languageBox");
+        var oSelectedLanguage = oLanguageBox.getSelectedItem();
+        if (oSelectedLanguage) {
+          var sLanguageKey = oSelectedLanguage.getText();
+        } else {
+          MessageToast.show("Select Language");
+          return;
+        }
+
+        if (sProductKey) {
+          var oBusyDialog = new sap.m.BusyDialog();
+          oBusyDialog.open();
+
+          // sap.ui.core.BusyIndicator.show()
+
+          var oTable = this.getView().byId("metricTable");
+          await this.filterTable(oTable, sProductKey);
+          oTable.setVisible(true);
+
+          oBusyDialog.close();
+        }
+      }
+    });
+  }
+);
--- commercial-ui/app/services.cds
+++ commercial-ui/app/services.cds
@@ -1,2 +1,4 @@
 
 using from './commercial-ui/annotations';
+
+using from './addon-ui/annotations';
